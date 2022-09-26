9ad8402e775a87f18bd3be938201518fa6797001
xxavierargentino <xxavierargentino@hotmail.com>
2022-09-26 19:33:36 -0300

    List panel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 
 import 'package:fl_cliente_mediacion/providers/login_provider.dart';
+import 'package:fl_cliente_mediacion/ui/input_decorations.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -14,31 +15,35 @@ class HomeScreen extends StatelessWidget {
 
     return  SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          //shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
-          toolbarHeight: 70,
-          backgroundColor: Color(0xff94273E),
-          centerTitle: true,
-          //title: const Icon(Icons.workspaces_filled, size: 50),
-          title: CircleAvatar(
-            radius: 30,
-            backgroundColor: Colors.black,
-            backgroundImage: AssetImage('assets/logo_m.png'),
-          ),
-          leading: IconButton(
-            onPressed: (){},
-            icon: const Icon(Icons.menu), color: Colors.white,),
-          actions: [
-            IconButton(
-              onPressed: (){},
-              icon: const Icon(Icons.notification_add, color: Colors.white,)
-              )            
-          ],
-        ),
+        appBar: _appBarFijo(),
         body: const _SliverHome(),
       ),
     );
+  }
+
+  AppBar _appBarFijo() {
+    return AppBar(
+        elevation: 0,
+        //shape: RoundedRectangleBorder(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))),
+        toolbarHeight: 70,
+        backgroundColor: Color(0xff94273E),
+        centerTitle: true,
+        //title: const Icon(Icons.workspaces_filled, size: 50),
+        title: CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.black,
+          backgroundImage: AssetImage('assets/logo_m.png'),
+        ),
+        leading: IconButton(
+          onPressed: (){},
+          icon: const Icon(Icons.menu), color: Colors.white,),
+        actions: [
+          IconButton(
+            onPressed: (){},
+            icon: const Icon(Icons.notification_add, color: Colors.white,)
+            )            
+        ],
+      );
   }
 }
 
@@ -56,46 +61,81 @@ class _SliverHome extends StatelessWidget {
       slivers: [                
         SliverPersistentHeader(
           delegate: _MySliverPersistenHeader(max: maximo, min: minimo),
-          pinned: true,
+          pinned: true,          
           ),
         SliverList(
           delegate: SliverChildListDelegate([
-            Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.amberAccent,
-            ),
-            Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.redAccent,
-            ),
-            Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.greenAccent,
-            ),
-                  Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.amberAccent,
-            ),
-            Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.redAccent,
-            ),
-            Container(
-              height: 100,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-              color: Colors.greenAccent,
+             ExpansionPanelList.radio(
+               animationDuration: Duration(seconds: 1),
+              expandedHeaderPadding: EdgeInsets.all(10),
+              elevation: 3,
+              children: [
+                _expansionPanelEdition(),
+                _expansionPanelEstadoTramite(),
+                _historialTramites()
+              ],
             )
+            
+            
           ])
           )
 
       ],
     );
   }
+
+  ExpansionPanelRadio _expansionPanelEdition() {
+    return ExpansionPanelRadio(
+                canTapOnHeader: true,
+                value: "formulario",
+                headerBuilder: (context, isExpanded){
+                  return ListTile(
+                    leading: Icon(Icons.edit_note_outlined),
+                    title: Text('Edición de Trámite en Curso', style: Theme.of(context).textTheme.headline6),                      
+                  );
+                } ,
+                body: ListTile(
+                    title: Text('Ut consectetur laborum ut velit nostrud tempor. Esse officia minim exercitation Lorem in magna cupidatat. Minim elit veniam ex ex ad ullamco mollit nulla reprehenderit non aliquip ut.',
+                     style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold)),                      
+                  )
+                );
+  }
+
+
+ExpansionPanelRadio _expansionPanelEstadoTramite() {
+    return ExpansionPanelRadio(
+                canTapOnHeader: true,
+                value: "detalle",
+                headerBuilder: (context, isExpanded){
+                  return ListTile(
+                    leading: Icon(Icons.stacked_bar_chart_outlined),
+                    title: Text('Estado del Trámite', style: Theme.of(context).textTheme.headline6),                      
+                  );
+                } ,
+                body: ListTile(
+                    title: Text('Excepteur officia labore fugiat reprehenderit reprehenderit. Lorem laboris aliquip ipsum labore nisi eiusmod nostrud sit enim id. Do nulla est officia consequat in qui consequat irure labore magna nulla. Sit velit fugiat dolor aliquip culpa ipsum ut occaecat ex anim aute ipsum ullamco officia. Eiusmod ad exercitation dolore eu aliqua exercitation enim est eiusmod in. Laborum ut sit officia id eu incididunt proident. Elit ut ex aliqua officia consequat elit in ullamco ut ullamco.Elit sunt magna reprehenderit ad et et nisi aliquip elit consequat. Irure sunt nostrud sit voluptate mollit cillum Lorem est laboris quis. Sint deserunt in quis ad laborum. Id sint cillum aute ut duis veniam fugiat. Mollit laboris ea consequat id id. Laborum enim magna anim nulla eu incididunt et.Anim esse amet id laboris sit sunt. Cupidatat et duis nulla adipisicing sint. Occaecat exercitation laboris voluptate dolore id incididunt nulla. Qui magna consectetur laboris elit deserunt.Ut consectetur laborum ut velit nostrud tempor. Esse officia minim exercitation Lorem in magna cupidatat. Minim elit veniam ex ex ad ullamco mollit nulla reprehenderit non aliquip ut.',
+                     style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),                      
+                  )
+                );
+  }
+
+  ExpansionPanelRadio _historialTramites() {
+    return ExpansionPanelRadio(
+                canTapOnHeader: true,
+                value: "historial",
+                headerBuilder: (context, isExpanded){
+                  return ListTile(
+                    leading: Icon(Icons.history),
+                    title: Text('Historial de Trámites', style: Theme.of(context).textTheme.headline6),                      
+                  );
+                } ,
+                body: ListTile(
+                    title: Text('Excepteur officia labore fugiat reprehenderit reprehenderit. Lorem laboris aliquip ipsum labore nisi eiusmod nostrud sit enim id. Do nulla est officia consequat in qui consequat irure labore magna nulla. Sit velit fugiat dolor aliquip culpa ipsum ut occaecat ex anim aute ipsum ullamco officia. Eiusmod ad exercitation dolore eu aliqua exercitation enim est eiusmod in. Laborum ut sit officia id eu incididunt proident. Elit ut ex aliqua officia consequat elit in ullamco ut ullamco.Elit sunt magna reprehenderit ad et et nisi aliquip elit consequat. Irure sunt nostrud sit voluptate mollit cillum Lorem est laboris quis. Sint deserunt in quis ad laborum. Id sint cillum aute ut duis veniam fugiat. Mollit laboris ea consequat id id. Laborum enim magna anim nulla eu incididunt et.Anim esse amet id laboris sit sunt. Cupidatat et duis nulla adipisicing sint. Occaecat exercitation laboris voluptate dolore id incididunt nulla. Qui magna consectetur laboris elit deserunt.Ut consectetur laborum ut velit nostrud tempor. Esse officia minim exercitation Lorem in magna cupidatat. Minim elit veniam ex ex ad ullamco mollit nulla reprehenderit non aliquip ut.',
+                     style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold)),                      
+                  )
+                );
+  }
+
 }
 
 class _MySliverPersistenHeader  extends SliverPersistentHeaderDelegate {
